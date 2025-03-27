43680d359179a2b459e2ae40ff4681ab09565d17
Loc90453 <huylochp2003@gmail.com>
2025-03-27 11:24:30 +0700

    sửa db + thêm khách hàng

---
+++ app/Filament/Resources/KhachHangResource/Pages/ListKhachHangs.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Resources\KhachHangResource\Pages;
+
+use App\Filament\Resources\KhachHangResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListKhachHangs extends ListRecords
+{
+    protected static string $resource = KhachHangResource::class;
+
+    protected static ?string $title = 'Danh sách khách hàng';
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make()
+                ->icon('heroicon-o-plus')
+                ->label('Tạo mới'),
+
+            Actions\CreateAction::make()
+                ->label('xuất excel')
+                ->icon('heroicon-o-arrow-down-tray')
+                ->color('primary')
+        ];
+    }
+}
