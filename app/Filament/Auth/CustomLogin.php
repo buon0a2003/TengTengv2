c7c1b6fff080011a3400ccc1b2700f456d898a14
Loc90453 <huylochp2003@gmail.com>
2025-03-31 11:26:24 +0700

    account suspended + sửa db

--- app/Filament/Auth/CustomLogin.php
+++ app/Filament/Auth/CustomLogin.php
@@ -2,10 +2,14 @@
 
 namespace App\Filament\Auth;
 
+use Filament\Facades\Filament;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Pages\Auth\Login;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
 use Illuminate\Validation\ValidationException;
+use Filament\Http\Responses\Auth\Contracts\LoginResponse;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 
 class CustomLogin extends Login
 {
@@ -51,5 +55,44 @@ class CustomLogin extends Login
         ]);
     }
 
+    public function authenticate(): ?LoginResponse
+    {
+        try {
+            $this->rateLimit(5);
+        } catch (TooManyRequestsException $exception) {
+            $this->getRateLimitedNotification($exception)?->send();
+
+            return null;
+        }
+
+        $data = $this->form->getState();
+
+        if (! Filament::auth()->attempt($this->getCredentialsFromFormData($data), $data['remember'] ?? false)) {
+            $this->throwFailureValidationException();
+        }
+
+        $user = Filament::auth()->user();
+
+        if (
+            ($user instanceof FilamentUser) &&
+            (! $user->canAccessPanel(Filament::getCurrentPanel()))
+        ) {
+            Filament::auth()->logout();
+
+            $this->throwFailureValidationException();
+        } elseif (
+            $user->Active == 0
+        ) {
+            Filament::auth()->logout();
+
+            throw ValidationException::withMessages([
+                'data.login' => __('Your account is suspended.'),
+            ]);
+        }
+
+        session()->regenerate();
+
+        return app(LoginResponse::class);
+    }
 
 }
