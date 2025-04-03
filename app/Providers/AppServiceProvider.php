7193b6baad1c6bd0903bdce6b4a409c265a2a212
Phanly <buon0a2003@gmail.com>
2025-04-03 14:30:22 +0700

    .

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,5 +41,7 @@ class AppServiceProvider extends ServiceProvider
             $table
                ->striped();
         });
+
+        ExportAction::configureUsing(fn (ExportAction $action) => $action->fileDisk('s3'));
     }
 }
