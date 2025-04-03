ee6fcb25494fd4b5f023c8969281d6f8721bb8c1
Phanly <buon0a2003@gmail.com>
2025-04-03 09:04:56 +0700

    fix export

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Providers;
 
 use App\Filament\Resources\UserResource\Pages\EditUser;
+use Filament\Actions\ExportAction;
 use Filament\Facades\Filament;
 use Filament\Navigation\NavigationItem;
 use Filament\Pages\Page;
@@ -39,5 +40,7 @@ class AppServiceProvider extends ServiceProvider
             $table
                ->striped();
         });
+
+        ExportAction::configureUsing(fn (ExportAction $action) => $action->fileDisk('public'));
     }
 }
