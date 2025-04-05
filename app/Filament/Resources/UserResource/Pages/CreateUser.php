9479e557bbb59c59c66309d902e43719ca0371d1
Loc90453 <huylochp2003@gmail.com>
2025-04-05 17:36:45 +0700

    gửi mail khi tạo tk

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -7,9 +7,42 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\CreateAndRedirectToIndex;
 use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Mail;
+
+/* copy đoạn này vào .env để chạy
+MAIL_MAILER=smtp
+MAIL_SCHEME=null
+MAIL_HOST=smtp.gmail.com
+MAIL_PORT=587
+MAIL_USERNAME=nt093225@gmail.com
+MAIL_PASSWORD=grtudybwbhksvsyk
+MAIL_FROM_ADDRESS="nt093225@gmail.com"
+MAIL_FROM_NAME="${APP_NAME}"
+*/
 
 class CreateUser extends CreateAndRedirectToIndex
 {
     protected static ?string $title = 'Tạo mới';
     protected static string $resource = UserResource::class;
+
+    protected string $plainPassword = '';
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $this->plainPassword = $data['password'];
+
+        $data['password'] = bcrypt($data['password']);
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $user = $this->record;
+
+        Mail::raw("Tài khoản của bạn đã được tạo:\n\nEmail: {$user->email}\nMật khẩu: {$this->plainPassword}", function ($message) use ($user) {
+            $message->to($user->email)
+                ->subject('Thông tin tài khoản');
+        });
+    }
 }
