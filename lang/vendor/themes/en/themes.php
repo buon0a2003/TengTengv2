f620709743190e1af14e55c0f513a5cd4362e77f
Loc90453 <huylochp2003@gmail.com>
2025-05-22 13:43:39 +0700

    việt hóa themes

---
+++ lang/vendor/themes/en/themes.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'themes' => 'Giao diện',
+    'primary_color' => 'Màu chủ đạo',
+    'select_base_color' => 'Chọn màu cơ bản cho giao diện của bạn',
+    'custom' => 'Tùy chỉnh',
+    'no_changing_primary_color' => 'Giao diện này không hỗ trợ thay đổi màu chủ đạo.',
+    'support_changing_primary_color' => 'Giao diện này hỗ trợ thay đổi màu chủ đạo.',
+    'select_interface' => 'Chọn kiểu hiển thị bạn muốn sử dụng.',
+    'select' => 'Chọn',
+    'light' => 'Sáng',
+    'dark' => 'Tối',
+    'active' => 'Đang sử dụng',
+    'primary_color_set' => 'Đã đặt màu chủ đạo giao diện là',
+    'theme_set_to' => 'Giao diện đã chuyển sang',
+    'appearance' => 'Giao diện hiển thị',
+    'no_light_mode' => 'Giao diện này không hỗ trợ chế độ sáng.',
+    'no_dark_mode' => 'Giao diện này không hỗ trợ chế độ tối.',
+    'support_light_mode' => 'Giao diện này hỗ trợ chế độ sáng.',
+    'support_dark_mode' => 'Giao diện này hỗ trợ chế độ tối.',
+    'theme_active' => 'Giao diện này đang được sử dụng.',
+];
